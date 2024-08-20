dff62de071d6a4d455fb514c462ca7e845b3bd87
James Godden <jgodden90@outlook.com>
2024-08-19 23:08:13 -0400

    created simple non parameterized function calls

--- StartVMs/run.ps1
+++ StartVMs/run.ps1
@@ -6,54 +6,65 @@ using namespace System.Net
 # Input bindings are passed in via param block.
 param($Request, $TriggerMetadata)
 
+$ResourceGroupName = "Training-BGP_Lab_RG_12"
+
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed a request."
 
 $body = "This HTTP triggered function has been executed.`n`n"
 
-# Validate query parameters.
+# # Validate query parameters.
 
-if (Get-AzResourceGroup -Name $Request.Query.ResourceGroupName) {
-    $ResourceGroupName = $Request.Query.ResourceGroupName
-}
-else {
-    $body = $body +
-'''
+# if (Get-AzResourceGroup -Name $Request.Query.ResourceGroupName) {
+#     $ResourceGroupName = $Request.Query.ResourceGroupName
+# }
+# else {
+#     $body = $body +
+# '''
 
-Exception: A valid Resource Group name was not provided.  
-Pass a Resource Group name in the URL after the "?ResourceGroupName=" to start or stop the Virtual Machines.
+# Exception: A valid Resource Group name was not provided.  
+# Pass a Resource Group name in the URL after the "?ResourceGroupName=" to start or stop the Virtual Machines.
 
-'''
-}
+# '''
+# }
 
-if (($Request.Query.Action -eq "Start") -or ($Request.Query.Action -eq "Stop")) {
-    $Action = $Request.Query.Action
-}
-else {
-    $body = $body +
-'''
+# if (($Request.Query.Action -eq "Start") -or ($Request.Query.Action -eq "Stop")) {
+#     $Action = $Request.Query.Action
+# }
+# else {
+#     $body = $body +
+# '''
 
-Exception: A valid Action was not provided.  
-Pass either Start or Stop in the URL after the "&Action=" to start or stop the Virtual Machines.
+# Exception: A valid Action was not provided.  
+# Pass either Start or Stop in the URL after the "&Action=" to start or stop the Virtual Machines.
 
-'''
-}
+# '''
+# }
+
+# $AzVMs = Get-AzvM -ResourceGroupName $ResourceGroupName
+
+# switch ($Action) {
+#     "Start" { 
+#         foreach ($AzVM in $AzVMs) {
+#             Start-AzVM -ResourceGroupName $ResourceGroupName -Name $AzVM.Name -NoWait
+#             $body = $body + "Starting Azure Virtual Machine `"$($AzVM.Name)`"`n"
+#         }
+#     }
+#     "Stop" {
+#         foreach ($AzVM in $AzVMs) {
+#             Stop-AzVM -ResourceGroupName $ResourceGroupName -Name $AzVM.Name -NoWait -Force
+#             $body = $body + "Stopping Azure Virtual Machine `"$($AzVM.Name)`"`n"
+#         }
+#     }
+# }
 
 $AzVMs = Get-AzvM -ResourceGroupName $ResourceGroupName
 
-switch ($Action) {
-    "Start" { 
-        foreach ($AzVM in $AzVMs) {
-            Start-AzVM -ResourceGroupName $ResourceGroupName -Name $AzVM.Name -NoWait
-            $body = $body + "Starting Azure Virtual Machine `"$($AzVM.Name)`"`n"
-        }
-    }
-    "Stop" {
-        foreach ($AzVM in $AzVMs) {
-            Stop-AzVM -ResourceGroupName $ResourceGroupName -Name $AzVM.Name -NoWait -Force
-            $body = $body + "Stopping Azure Virtual Machine `"$($AzVM.Name)`"`n"
-        }
-    }
+$body = $body + "`nStarting the following Virtual Machines in Resource Group ${ResourceGroupName}:`n`n"
+
+foreach ($AzVM in $AzVMs) {
+    Start-AzVM -ResourceGroupName $ResourceGroupName -Name $AzVM.Name -NoWait
+    $body = $body + "$($AzVM.Name)`n"
 }
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
